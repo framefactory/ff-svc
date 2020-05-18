1916efa39329383b39a8025c8913ca3a370360a9
Ralph Wiedemeier <ralph@framefactory.ch>
2020-05-18 09:31:21 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FF Svelte Component Library
+
+Copyright 2020 [Frame Factory GmbH](https://framefactory.ch), [Ralph Wiedemeier](https://about.me/ralphw) 
---
+++ source/Button.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="typescript">
+</script>
+
+<script lang="typescript">
+</script>
+
+<button>
+    Hello Svelte!
+</button>
+
+<style type="text/scss">
+    button {
+        background-color: red;
+    }
+</style>
